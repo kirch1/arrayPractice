5b6c4429751ea63ebfed91e7cbe0efab9669cc62
Jason Kirchman <kirchman.jason@icloud.com>
2022-07-09 01:10:49 -0500

    Call different array methods on all array varables

--- arrays.js
+++ arrays.js
@@ -1,3 +1,15 @@
 var nflTeams = ['Chiefs','Broncos,','Chargers','Raiders']
 var playerNumbers = [15,87,11,19]
-var madePlayoffs - [true,false,false,false]
+var hasWonSuperBowl = [true,true,false,true]
+
+//remove and print the best team from the nflTeams array, leaving only the bad teams in the array
+console.log(nflTeams.shift())
+console.log(nflTeams)
+
+//print the playerNumbers array, add an element to the beginning, reprint to confirm
+console.log(playerNumbers)
+playerNumbers.unshift(25)
+console.log(playerNumbers)
+
+//print the middle two elements of the hasWonSuperBowl array
+console.log(hasWonSuperBowl.slice(1,3))
