5d6e88bad0989bba6cffb085a806f54a09f062c2
Jason Kirchman <kirchman.jason@icloud.com>
2022-07-09 01:28:02 -0500

    Add code to demonstrate understanding of index positions

--- arrays.js
+++ arrays.js
@@ -3,6 +3,7 @@ var playerNumbers = [15,87,11,19]
 var hasWonSuperBowl = [true,true,false,true]
 
 //remove and print the best team from the nflTeams array, leaving only the bad teams in the array
+//call method on hasWonSuperBowl also so the two arrays match
 console.log(nflTeams.shift())
 console.log(nflTeams)
 
@@ -13,3 +14,9 @@ console.log(playerNumbers)
 
 //print the middle two elements of the hasWonSuperBowl array
 console.log(hasWonSuperBowl.slice(1,3))
+
+//assuming the index for each team in the nflTeams array corresponds to the hasWonSuperBowl array, print if the Chargers have ever won the super bowl
+console.log(`The ${nflTeams[1]} have won the super bowl: ${hasWonSuperBowl[1]}`)
+
+//print the jersey number of the Chiefs Tight End
+console.log(playerNumbers[2]);
